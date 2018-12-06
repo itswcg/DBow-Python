c303d377c8028b2dabf33b546cd9f756053b61b1
Wcg <31924960+itswcg@users.noreply.github.com>
2018-12-06 17:15:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from voc_tree import constructTree
 from matcher import *
 
 
-N = 20 #训练字典图片的数量
-K = 12 #聚类K类
-L = 5 #字典树L层
-n = 21 #测试的图片的数量
-T = 1 #相似度阈值
+N = 20  # 训练字典图片的数量
+K = 12  # 聚类K类
+L = 5  # 字典树L层
+n = 21  # 测试的图片的数量
+T = 1  # 相似度阈值
 
-image_descriptors = orb_features(N) #提取特征
+image_descriptors = orb_features(N)  # 提取特征
 # print image_descriptors
 
 FEATS = []
@@ -20,37 +20,29 @@ FEATS = []
 for feats in image_descriptors:
     FEATS += [np.array(fv, dtype='float32') for fv in feats]
 
-FEATS = np.vstack(FEATS) #将特征转化为np的数组
-# print FEATS
+FEATS = np.vstack(FEATS)  # 将特征转化为np的数组
 
-treeArray = constructTree(K, L, np.vstack(FEATS)) #建立字典树，并打印结果
+
+treeArray = constructTree(K, L, np.vstack(FEATS))  # 建立字典树，并打印结果
 tree = Tree(K, L, treeArray)
-# tree.build_tree(N, image_descriptors)
-# print "the vector of image:"
-# print tree.transform(2)
-# print tree.imageIDs, tree.dbLengths
 
-# matcher = Matcher(N, image_descriptors, tree)
-# # print matcher.query(4)
+matcher = Matcher(N, image_descriptors, tree)
 
-# # add images
+# add images
+for i in range(n):
+    des = update_image(i)
+    tree.update_tree(i, des)
 
-# for i in range(n):
-#     des = update_image(i)
-#     tree.update_tree(i, des)
-# # print tree.imageIDs
-
-# # 比较
-#     print "{}.jpg compute cosine similarity:".format(i)
-#     res = {}
-#     for j in range(tree.N-1):
-#         print 'Image {} vs Image {}: {}'.format(i, j, matcher.cos_sim(tree.transform(i), tree.transform(j)))
-#         if matcher.cos_sim(tree.transform(i), tree.transform(j)) >= T:
-#             res[j] = matcher.cos_sim(tree.transform(i), tree.transform(j))
-#     if res:
-#         r = max(res.items(), key=lambda x:x[1])[0]
-#         print ("相似度最高的图片为{}.jpg".format(r))
-#         print tree.transform(r)
-#     else:
-#         print("None")
-
+# 比较
+print "{}.jpg compute cosine similarity:".format(i)
+res = {}
+for j in range(tree.N - 1):
+    print 'Image {} vs Image {}: {}'.format(i, j, matcher.cos_sim(tree.transform(i), tree.transform(j)))
+    if matcher.cos_sim(tree.transform(i), tree.transform(j)) >= T:
+        res[j] = matcher.cos_sim(tree.transform(i), tree.transform(j))
+if res:
+    r = max(res.items(), key=lambda x: x[1])[0]
+    print("相似度最高的图片为{}.jpg".format(r))
+    print tree.transform(r)
+else:
+    print("None")
